ddae8fb007bc374457fc9fbc2182ecf78783df4b
Sandra Papp <sandra@praktika1.cs.tlu.ee>
2017-12-08 13:14:26 +0200

    muutus

--- sandra_joonised.py
+++ sandra_joonised.py
@@ -8,9 +8,11 @@ plt.xticks([1, 2, 3], ["Juku", "Mati", "Kati"])
 plt.savefig("sandra.png")
 plt.clf()
 
+fig, ax=plt.subplots() #teksti jaoks eraldi kiht
 plt.scatter([70, 60, 65], [170, 160, 180])
 plt.title("laste andmed")
 plt.xlabel("mass")
 plt.ylabel("pikkus")
+ax.annotate("Kati", (60, 160))
 plt.savefig("sandra2.png")
 plt.clf()
