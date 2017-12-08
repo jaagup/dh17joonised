b206b0a49a5fa74decf7270f94908b3e74cabf43
Liisi Pütsepp <liisi@praktika1.cs.tlu.ee>
2017-12-08 12:53:42 +0200

    muutus

--- liisi_joonised.py
+++ liisi_joonised.py
@@ -3,7 +3,7 @@ import matplotlib
 print("Tere, Liisi!!")
 matplotlib.use("Agg")  #et suudaks teksitekraanil hakkama saada
 import matplotlib.pyplot as plt
-plt.bat([1, 2, 3],[170, 160, 180])
-plt.xticks([1, 2, 3],["juku", "kati", mati"])
+plt.bar([1, 2, 3],[170, 160, 180])
+plt.xticks([1, 2, 3],["juku", "kati", "mati"])
 plt.savefig("liisi.png")
 plt.clf()
